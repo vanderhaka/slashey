1fcc1c1afeefe3391ce1448a1c21fa649c3aac6a
James Vanderhaak <jamesvanderhaak@gmail.com>
2025-12-11 15:03:27 +1030

    Initial Commit

---
+++ Slashey/SlasheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlasheyApp.swift
+//  Slashey
+//
+//  Created by James Vanderhaak on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct SlasheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SlasheyTests/SlasheyTests.swift
@@ -0,0 +1,17 @@
+//
+//  SlasheyTests.swift
+//  SlasheyTests
+//
+//  Created by James Vanderhaak on 11/12/2025.
+//
+
+import Testing
+@testable import Slashey
+
+struct SlasheyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
